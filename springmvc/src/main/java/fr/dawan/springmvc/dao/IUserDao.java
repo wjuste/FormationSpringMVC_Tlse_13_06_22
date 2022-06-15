e3083de6f0ada7f343fd2057cae0a102314e5586
Woodson Juste <j.woodson@hotmail.fr>
2022-06-15 17:02:12 +0200

    Spring MVC (web.xml - entity - dao...)

--- springmvc/src/main/java/fr/dawan/springmvc/dao/IUserDao.java
+++ springmvc/src/main/java/fr/dawan/springmvc/dao/IUserDao.java
@@ -5,6 +5,7 @@ import fr.dawan.springmvc.entities.User;
 public interface IUserDao extends EntityRepository<User>{
 	
 	public User findUserByPasswordAndEmail(String email, String password);
-
+	
+	
 
 }
