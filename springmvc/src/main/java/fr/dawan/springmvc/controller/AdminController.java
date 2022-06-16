bee7b603aebcb65c1defc2a2b1ad4950758c6d8a
Woodson Juste <j.woodson@hotmail.fr>
2022-06-16 17:59:47 +0200

    AdminController  -  users.jsp  : lstUser and deleteUser

--- springmvc/src/main/java/fr/dawan/springmvc/controller/AdminController.java
+++ springmvc/src/main/java/fr/dawan/springmvc/controller/AdminController.java
@@ -1,10 +1,17 @@
 package fr.dawan.springmvc.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import fr.dawan.springmvc.entities.User;
 import fr.dawan.springmvc.service.IUserService;
 
 
@@ -20,10 +27,47 @@ public class AdminController {
 	@Autowired
 	private IUserService service;
 	
+//	@RequestMapping(value="/users", method = RequestMethod.GET)
+//	public String lstUser() {
+//		return "users";
+//	}
 	
-	@RequestMapping(value="/users", method = RequestMethod.GET)
-	public String lstUser() {
-		return "users";
+	@GetMapping("/users")
+	public ModelAndView lstUser() {
+		
+		ModelAndView modelView = new ModelAndView();
+		List<User> users = service.findAllUsers();
+		modelView.setViewName("users");
+		modelView.addObject("users", users);
+		
+		return modelView;
 	}
+	
+	
+	/*
+	 * Admin pourra effacer un user par son id 
+	 * 
+	 * @PathVariable : 
+	 * @PathVariable est une annotation Spring qui indique qu'un paramètre de méthode doit être lié à une variable de la 
+	 * requête (url)
+	 */
+	@GetMapping("/users/delete/{id}")
+	public String deleteUser(Model model, @PathVariable Long id) {
+		User user = service.getUser(id);
+		if(user != null) {
+			service.deleteUser(id);
+		}
+		return "redirect:/admin/users";
+	}
+	
+	
+	
+	//Ajouter un utiliser 
+		//Afficher le formulaire  (GET)
+		//Soummettre le formulaire (POST)
+	//Pour la validation : Mot clé @Valid @ModelAttribute 
+	
+	//url ==> users/add
+	
 
 }
--- springmvc/src/main/webapp/WEB-INF/views/users.jsp
+++ springmvc/src/main/webapp/WEB-INF/views/users.jsp
@@ -1,12 +1,48 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="ISO-8859-1">
-<title>Insert title here</title>
+<meta charset="utf-8">
+<title>Liste des utilisateurs</title>
+<!--  context est utilisé pour sépcifier l'application (ou le nom du projet)-->
+<link rel="stylesheet" href="<c:url value="/webjars/bootstrap/4.3.1/css/bootstrap.min.css" context="/springmvc"/>"/>
 </head>
 <body>
-	Test
+	<h1 class="mb-5">Liste des utilisateurs</h1>
+	
+	<table class="table table-hover col-xl-6 col-lg-6" style="margin-left:20px">
+		<thead class="card-header" style="background-color:#C887D">
+			<tr>
+				<th>Prenom</th>
+				<th>Nom</th>
+				<th>Email</th>
+				<th>Role</th>
+				<th></th>
+			</tr>
+		</thead>
+		<tbody  style="background-color:#F5F6F2">
+		<c:forEach var="u" items="${users}">
+			<tr>
+				<td>${u.prenom}</td>
+				<td>${u.nom}</td>
+				<td>${u.email}</td>
+				<td>${u.role}</td>
+				
+				<!-- var: Nom de la variable pour stocker l'url formatée (url résultante) 
+				"/admin/users/delete/1"  dont ${u.id} = 1
+				-->
+				<td>
+					<c:url  value="/admin/users/delete/${u.id}"  context="/springmvc" var="urldelete" />
+					<a href="${urldelete}"  onclick="return confirm('Are you sure?')"> Delete</a>
+				</td>
+			</tr>
+			
+		</c:forEach>
+		</tbody>
+	</table>
 </body>
 </html>
