568a47a220dcb1b4a4fbdf6d0c80421d1da62a95
Woodson Juste <j.woodson@hotmail.fr>
2022-06-17 14:20:13 +0200

    Creation projet Spring boot + entités BIS

---
+++ yourBank31/src/main/java/fr/dawan/yourbank31/YourBank31Application.java
@@ -0,0 +1,13 @@
+package fr.dawan.yourbank31;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourBank31Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourBank31Application.class, args);
+	}
+
+}
---
+++ yourBank31/src/test/java/fr/dawan/yourbank31/YourBank31ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.dawan.yourbank31;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourBank31ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
